9f4e9f51d2fb78b8191179f9b54deecbf562bea3
Matt Fuller <mnfuller552@gmail.com>
2023-04-10 17:26:12 -0400

    finished project

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+
+		private double balance = 0;
+
+		
+
+		public void Deposit(double depositAmount)
+		{
+			balance = depositAmount;
+			
+		}
+
+		public double GetBalance()
+		{
+			return balance;
+		}
+
+       
+    }
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,9 +2,28 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+       
+
+            static void Main(string[] args)
+            {
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            double depositAmount = double.Parse(Console.ReadLine());
+
+            account.Deposit(depositAmount);
+
+            Console.WriteLine($"Thank you! Your balance is now ${account.GetBalance()}");
+
+
+
+
+
+
+               
+            }
+
+        
     }
 }
